e42313286914555ddc608f8b0d975ecd5e9e8e72
bok-chris <cpfermin@gmail.com>
2023-08-22 16:39:08 +0800

    Exercise 1.1-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,66 @@
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Header = (props) => <h1>{props.name}</h1>;
+
+const Part = (props) => (
+  <>
+    <h2>{props.part.name}</h2>
+    <p>
+      There are <b>{props.part.exercises}</b> exercises in this part of the
+      course.
+    </p>
+  </>
+);
+
+const Content = (props) => (
+  <>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </>
+);
+
+const Total = (props) => {
+  let total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
 
   return (
-    <div>
-      <h1>{course}</h1>
+    <>
       <p>
-        {part1} {exercises1}
+        There are <b>{total}</b> exercises in total.
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
   );
 };
 
